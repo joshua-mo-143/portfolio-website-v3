7a27f2398479e87ef1738c719b32c8510fb0ee6c
Josh Mo <joshua.om.876@gmail.com>
2022-10-13 08:40:34 +0100

    push new project

--- src/projectData.js
+++ src/projectData.js
@@ -1,14 +1,6 @@
 export const projectData = [
     {
         id: 1,
-        name: "Project Management app mockup",
-        desc: "A front end mockup for a project management app to streamline internal processes.",
-        stack: ['React', 'TailwindCSS'],
-        github: "https://github.com/joshua-mo-143/management-app-frontend",
-        live: "https://management-app-frontend.vercel.app/"
-    },
-    {
-        id: 2,
         name: "Fashion Community website",
         desc: "A front-end mockup for a community website for people to post their outfits and find like minded people as well as having competitions.",
         stack: ['React', 'SCSS'],
@@ -17,7 +9,7 @@ export const projectData = [
     },
 
     {
-        id: 3,
+        id: 2,
         name: "E-shop Design",
         desc: "An e-shop design I created using React. Has a working cart and checkout menu.",
         stack: ['React', 'SCSS'],
@@ -26,12 +18,31 @@ export const projectData = [
     },
 
     {
-        id: 4,
+        id: 3,
         name: "Brochure Website",
         desc: "A website design created to act as the face of a business for introducing prospective customers to the brand.",
         stack: ['React', 'Typescript', 'TailwindCSS', 'Blender'],
         github: "https://github.com/joshua-mo-143/r3f-landingpage",
         live: "https://r3f-landingpage.vercel.app/"
-    }
+    },
+
+    {
+        id: 4,
+        name: "Project Management app mockup",
+        desc: "A front end mockup for a project management app to streamline internal processes.",
+        stack: ['React', 'TailwindCSS'],
+        github: "https://github.com/joshua-mo-143/management-app-frontend",
+        live: "https://management-app-frontend.vercel.app/"
+    },
+
+    {
+        id: 5,
+        name: "Stripe Test Checkout",
+        desc: "An app that makes Stripe test payments.",
+        stack: ['Node', 'Express'],
+        github: "https://github.com/joshua-mo-143/stripe-test-payments",
+        live: "https://powerful-jade-brown-bear.cyclic.app/"
+    },
+
 
 ]
